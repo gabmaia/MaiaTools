4dcd49cae38097c85b275d29b272682865828975
Gabriel Maia <36666838+gabmaia@users.noreply.github.com>
2019-04-01 14:35:36 -0300

    Add files via upload

--- R/map_maia.R
+++ R/map_maia.R
@@ -9,6 +9,8 @@
 #'
 #' @param fill Coluna com os valores a serem plotados
 #'
+#' @param join Booleano, se T sera automaticamente carregado o dataset countries
+#'
 #' @param factor Booleano, T se fill for Factor
 #'
 #' @param ncolors Numero de cores a ser usado se factor==T
@@ -25,7 +27,13 @@
 #'
 
 
-map_maia  <- function(df, region, fill, factor=T, ncolors=7, is_small="is_small") {
+map_maia  <- function(df, region="region", fill=="n", join_countries=T, is_factor=T, ncolors=7,
+                      is_small="is_small") {
+
+
+  if(isTRUE(join_countries)){
+    df %>%rename("region"=!!as.name(region)) %>%
+      left_join(MaiaTools::countries2, region)->df}
 
 
   df %>% rename("is_small2"=!!as.name(is_small)) %>%
@@ -40,7 +48,7 @@ map_maia  <- function(df, region, fill, factor=T, ncolors=7, is_small="is_small"
 
 
 
-    if(factor==T){
+    if(isTRUE(is_factor)){
       map_data("world") %>%
         mutate(region=ifelse(region=="French Guiana",
                              "France", ifelse(region=="Greenland", "Denmark",region))) %>%
@@ -50,7 +58,8 @@ map_maia  <- function(df, region, fill, factor=T, ncolors=7, is_small="is_small"
         geom_point(data=smol, aes(x=as.numeric(ponto_lon), y=as.numeric(ponto_lat),fill=fill),
                    color="gray12",shape=21, position = "dodge")+
         ggalt::coord_proj()+
-    scale_fill_manual(values =  (colorRampPalette(RColorBrewer::brewer.pal(4, "YlOrRd"))(ncolors)), drop=F)}else{
+    scale_fill_manual(values =  (colorRampPalette(RColorBrewer::brewer.pal(4, "YlOrRd"))(ncolors)),
+                      drop=F)}else{
     map_data("world") %>%
       mutate(region=ifelse(region=="French Guiana",
                            "France", ifelse(region=="Greenland", "Denmark",region))) %>%
@@ -60,8 +69,7 @@ map_maia  <- function(df, region, fill, factor=T, ncolors=7, is_small="is_small"
       geom_point(data=smol, aes(x=as.numeric(ponto_lon), y=as.numeric(ponto_lat),fill=fill),
                  color="gray12",shape=21, position = "dodge")+
       ggalt::coord_proj()+
-    scale_fill_continuous()
-  }
+    scale_fill_continuous()  }
 
 
 }
