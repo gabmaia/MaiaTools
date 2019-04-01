c25282e0c0be97b398f665123fac9ed6d255ce77
Gabriel Maia <36666838+gabmaia@users.noreply.github.com>
2019-04-01 14:11:21 -0300

    Add files via upload

---
+++ R/countries-data.R
@@ -0,0 +1,17 @@
+#' Country Names data
+#'
+#' Dataset com diversos padroes de nomes de paises
+#' Arabidopsis, with data on 162 recombinant inbred lines (Ler x
+#' Cvi). The outcome is the root tip angle (in degrees) at two-minute
+#' increments over eight hours.
+#'
+#' @docType data
+#'
+#' @usage data(countries)
+#'
+#' @format An object of class \code{"cross"}; see \code{\link[qtl]{read.cross}}.
+#'
+#' @keywords datasets
+#'
+#' 
+"countries"
